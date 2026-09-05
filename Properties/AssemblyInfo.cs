d4c972f0b96f9c4f25c077059187d8e84ee2147b
Phước Vinh <88916609+PhuocVinh308@users.noreply.github.com>
2026-09-05 22:44:34 +0700

    Set author & organization metadata to Tiệm vàng Kim Phong Thảo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,12 +3,19 @@ using System.Runtime.InteropServices;
 using Rhino.PlugIns;
 
 [assembly: Guid("E3F91A20-9D7A-413F-9CE6-2AF15B5C4B9A")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: AssemblyTitle("KPT")]
-[assembly: AssemblyDescription("Phần mềm Bản Đồ Đá - Tiệm vàng Kim Phong Thảo")]
+[assembly: AssemblyDescription("Phần mềm Bản Đồ Đá & Tính Khối Lượng - Tiệm vàng Kim Phong Thảo")]
 [assembly: AssemblyCompany("Tiệm vàng Kim Phong Thảo")]
+[assembly: AssemblyProduct("KPT Plugin - Tiệm vàng Kim Phong Thảo")]
+[assembly: AssemblyCopyright("Copyright © Tiệm vàng Kim Phong Thảo")]
+[assembly: AssemblyTrademark("Tiệm vàng Kim Phong Thảo")]
+
 [assembly: PlugInDescription(DescriptionType.Organization, "Tiệm vàng Kim Phong Thảo")]
+[assembly: PlugInDescription(DescriptionType.Address, "Tiệm vàng Kim Phong Thảo")]
+[assembly: PlugInDescription(DescriptionType.Country, "Việt Nam")]
 [assembly: PlugInDescription(DescriptionType.WebSite, "kimphongthao.com")]
 [assembly: PlugInDescription(DescriptionType.Email, "contact@kimphongthao.com")]
 [assembly: PlugInDescription(DescriptionType.UpdateUrl, "kimphongthao.com")]
+
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: AssemblyVersion("1.0.0.1")]
